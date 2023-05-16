289f0c30ef11218bad580ae9fafdf0311531ea67
Dra9ana <69038885+Dra9ana@users.noreply.github.com>
2023-05-16 16:42:04 +0200

    Add files via upload

---
+++ SRV_zad_5/Debug/main.d
@@ -0,0 +1,117 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/FreeRTOS.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stddef.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOSConfig.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/projdefs.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/portable.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/deprecated_definitions.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/portable/CCS/MSP430X/portmacro.h
+main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/mpu_wrappers.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/task.h
+main.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/list.h
+main.obj: ../../common/ETF5529_HAL/hal_ETF_5529.h
+main.obj: ../../common/ETF5529_HAL/hal_board.h
+main.obj: ../../common/ETF5529_HAL/hal_led.h
+main.obj: ../../common/ETF5529_HAL/hal_7seg.h
+main.obj: ../../common/drivers/MSP430F5xx_6xx/pmm.h
+main.obj: ../../common/drivers/MSP430F5xx_6xx/inc/hw_memmap.h
+main.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h
+main.obj: ../../common/drivers/MSP430F5xx_6xx/ucs.h
+
+../main.c:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdio.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdarg.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/FreeRTOS.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stddef.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOSConfig.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/projdefs.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/portable.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/deprecated_definitions.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/portable/CCS/MSP430X/portmacro.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/mpu_wrappers.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/task.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_5/FreeRTOS_source/include/list.h:
+
+../../common/ETF5529_HAL/hal_ETF_5529.h:
+
+../../common/ETF5529_HAL/hal_board.h:
+
+../../common/ETF5529_HAL/hal_led.h:
+
+../../common/ETF5529_HAL/hal_7seg.h:
+
+../../common/drivers/MSP430F5xx_6xx/pmm.h:
+
+../../common/drivers/MSP430F5xx_6xx/inc/hw_memmap.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdbool.h:
+
+../../common/drivers/MSP430F5xx_6xx/ucs.h:
+
---
+++ SRV_zad_5/main.c
@@ -0,0 +1,139 @@
+/**
+ * @file    main.c
+ * @author  Haris Turkmanovic(haris@etf.rs)
+ * @date    2021
+ * @brief   SRV Zadatak 3
+ *
+ */
+
+/* Standard includes. */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Hardware includes. */
+#include "msp430.h"
+
+/* User include */
+#include "../common/ETF5529_HAL/hal_ETF_5529.h"
+
+/** Task 1 Priority */
+#define mainTAKS_1_PRIO        ( 1 )
+/** Task 2 Priority */
+#define mainTAKS_2_PRIO        ( 2 )
+
+static void prvSetupHardware( void );
+static void prvTask1Function( void *pvParameters );
+static void prvTask2Function( void *pvParameters );
+
+/**
+ * @brief Task 1 function
+ *
+ * This task check if button is pressed and change state of diode
+ * LD3. At the end, task is blocked for 200 Ticks
+ */
+static void prvTask1Function( void *pvParameters )
+{
+    static uint16_t cnt = 0;
+    uint16_t i;
+    /*Initial button states are 1 because of pull-up configuration*/
+    uint8_t     previousButtonState = 1;
+    uint8_t     currentButtonState  = 1;
+    for ( ;; )
+    {
+        /*Read button state*/
+        currentButtonState = ((P1IN & 0x10) >> 4);//BIT4
+        /*Detected button signal edge*/
+        if( previousButtonState != currentButtonState){
+            for(i = 0; i < 1000; i++);
+            /*read button state again to verify rising edge of button signal*/
+            currentButtonState = ((P1IN & 0x10) >> 4);
+            previousButtonState = currentButtonState;
+            if(currentButtonState == 0){
+                /* If button is still pressed toggle diode LD3 */
+                cnt++;
+            }
+        }
+        /* delay for synchronization */
+        //vTaskDelay( 200 );
+
+        if(cnt == 3){
+            halTOGGLE_LED( LED3 );
+            cnt = 0;
+        }
+    }
+}
+/**
+ * @brief Task 2 function
+ *
+ * This task change state of diode LD 4. At the end, task is blocked
+ * for 100 Ticks
+ */
+static void prvTask2Function( void *pvParameters )
+{
+    /* low priority task that toggles LED D4 */
+    for ( ;; )
+    {
+        /* toggle LED D4 */
+        halTOGGLE_LED( LED4 );
+        /* delay for synchronization */
+        vTaskDelay( 100 );
+    }
+}
+/**
+ * @brief main function
+ */
+void main( void )
+{
+    /* Configure peripherals */
+    prvSetupHardware();
+
+    /* Create tasks */
+    if(xTaskCreate( prvTask1Function,
+                 "Task 1",
+                 configMINIMAL_STACK_SIZE,
+                 NULL,//no arguments
+                 mainTAKS_1_PRIO,
+                 NULL
+               ) != pdPASS) while(1);
+    if(xTaskCreate( prvTask2Function,
+                 "Task 2",
+                 configMINIMAL_STACK_SIZE,
+                 NULL,
+                 mainTAKS_2_PRIO,
+                 NULL
+               ) != pdPASS) while(1);
+
+    /* Start the scheduler. */
+    vTaskStartScheduler();
+
+    /* If all is well then this line will never be reached.  If it is reached
+    then it is likely that there was insufficient (FreeRTOS) heap memory space
+    to create the idle task.  This may have been trapped by the malloc() failed
+    hook function, if one is configured. */
+    for( ;; );
+}
+
+/**
+ * @brief Configure hardware upon boot
+ */
+static void prvSetupHardware( void )
+{
+    taskDISABLE_INTERRUPTS();
+
+    /* Disable the watchdog. */
+    WDTCTL = WDTPW + WDTHOLD;
+
+    hal430SetSystemClock( configCPU_CLOCK_HZ, configLFXT_CLOCK_HZ );
+
+    /* Init buttons */
+    P1DIR &= ~0x30;
+    P1REN |= 0x30;
+    P1OUT |= 0x30;
+
+    /* initialize LEDs */
+    vHALInitLED();
+}
