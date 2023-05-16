026b0403a465fe5f820f2b13ba06c0f98bba2be3
Dra9ana <69038885+Dra9ana@users.noreply.github.com>
2023-05-16 16:38:48 +0200

    Add files via upload

---
+++ SRV_zad_20/Debug/FreeRTOS_source/timers.d
@@ -0,0 +1,90 @@
+# FIXED
+
+FreeRTOS_source/timers.obj: ../FreeRTOS_source/timers.c
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/FreeRTOS.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stddef.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOSConfig.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/projdefs.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/portable.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/deprecated_definitions.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/portable/CCS/MSP430X/portmacro.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5529.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+FreeRTOS_source/timers.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/mpu_wrappers.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/task.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/list.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/queue.h
+FreeRTOS_source/timers.obj: C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/timers.h
+
+../FreeRTOS_source/timers.c:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdlib.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/FreeRTOS.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stddef.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOSConfig.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/projdefs.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/portable.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/deprecated_definitions.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/portable/CCS/MSP430X/portmacro.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/mpu_wrappers.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/task.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/list.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/queue.h:
+
+C:/Users/Dragana/Desktop/SRV_lab/Workspace/SRV_zad_20/FreeRTOS_source/include/timers.h:
+
